b79e44df66d50366eb206b5d09c04d9f5218ab50
piashsarker <sarkerpt@gmail.com>
2017-11-16 12:23:54 +0600

    package.json edit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.listen(process.env.PORT || 8081);
 
 app.get('/*',function (req, res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
+    console.log(__dirname+'/dist/index.html');
 });
 
 console.log("Angular Tour Of Heroes Running on PORT : "+ (process.env.PORT || 8081) );
